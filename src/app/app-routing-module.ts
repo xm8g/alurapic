684279746b5311154ec06343417365925057549d
Alex Ronchi Dutra <alex.dutra@serpro.gov.br>
2020-09-11 18:11:21 -0300

    second commit

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -3,11 +3,43 @@ import { Routes, RouterModule } from '@angular/router';
 import { PhotoListComponent } from './photo/photo-list/photo-list.component';
 import { PhotoFormComponent } from './photo/photo-form/photo-form.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { PhotoListService } from './photo/photo-list/photo-list.service';
+import { SigninComponent } from './home/signin/signin.component';
+import { AuthGuard } from './core/auth/auth.guard';
+import { SignupComponent } from './home/signup/signup.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  { path: 'user/:userName', component: PhotoListComponent },
-  { path: 'p/add', component: PhotoFormComponent },
-  { path: '**', component: NotFoundComponent }
+  {
+    path: '',
+    component: HomeComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: '',
+        component: SigninComponent,
+      },
+      {
+        path: 'signup',
+        component: SignupComponent,
+      },
+    ]
+  },
+  {
+    path: 'user/:userName',
+    component: PhotoListComponent,
+    resolve: {
+      photos: PhotoListService
+    }
+  },
+  {
+    path: 'p/add',
+    component: PhotoFormComponent
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
 ];
 
 
